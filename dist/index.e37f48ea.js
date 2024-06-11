ccda71bc2a568d2410df5129ab1f450903792ecc
Ramandeep Chamba <ramanchamba98@gmail.com>
2024-06-11 19:46:25 +0530

    Added pattern to pagination e.g. 1 ... 5 6 7 ... 11 when there are lot of pages

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -3349,29 +3349,79 @@ class PaginationView extends (0, _viewDefault.default) {
     _generateMarkup() {
         const numOfPages = Math.ceil(this._data.results.length / this._data.resultsPerPage);
         const curPage = this._data.page;
+        let markup = "";
+        // Min number of pages after which (1 2 3 4 ... 7) pattern applies
+        const minPageLimit = 6;
+        // 1 2 3 4 ... 25 or 1 ... 22 23 24 25 (for maxPagesTogether = 4)
+        const maxPagesTogether = 4;
         // console.log(this._data);
-        // Page 1 and there are other pages
-        if (curPage === 1 && numOfPages > 1) //   return '1st page, others';
-        // show only next button
-        return this._generateMarkupButton("next", curPage + 1);
-        // Last page
-        if (curPage === numOfPages && numOfPages > 1) // show only previous button
-        return this._generateMarkupButton("prev", curPage - 1);
-        // Other page
-        if (curPage < numOfPages && curPage > 1) //   return 'other page';
-        // show previous and next button
-        return this._generateMarkupButton("next", curPage + 1) + this._generateMarkupButton("prev", curPage - 1);
-        // Page 1 and there are NO other pages
-        return "";
+        // If only one page no pagination
+        if (numOfPages === 1) return markup;
+        // Generate previous button
+        markup += this._generatePrevNextButton("prev", curPage - 1, numOfPages);
+        // Generate button for each page (numOfPages)
+        // for (let i = 1; i <= numOfPages; i++) {
+        //   markup += this._generateMarkupButton(i, curPage);
+        // }
+        // Generate Pagination Pattern (1... 5 6 7 ... 11)
+        markup += this._generatePaginationPattern(curPage, numOfPages, minPageLimit, maxPagesTogether);
+        // Generate next button
+        markup += this._generatePrevNextButton("next", curPage + 1, numOfPages);
+        return markup;
     }
-    _generateMarkupButton(type, page) {
+    _generatePaginationPattern(curPage, numOfPages, minPageLimit, maxPagesTogether) {
+        let markup = "";
+        // 1 2 3 4 ... 25
+        if (curPage - maxPagesTogether < 1) {
+            if (numOfPages > minPageLimit) {
+                // render 1-4 (for maxPagesT = 4)
+                for(let i = 1; i <= maxPagesTogether; i++)markup += this._generateMarkupButton(i, curPage);
+                // ...
+                markup += `<span class='pagination__dots'>...</span>`;
+                // last
+                markup += this._generateMarkupButton(numOfPages, curPage);
+            } else // render 1-last
+            for(let i = 1; i <= numOfPages; i++)markup += this._generateMarkupButton(i, curPage);
+        } else if (curPage + maxPagesTogether > numOfPages) {
+            if (numOfPages > minPageLimit) {
+                // render 1
+                markup += this._generateMarkupButton(1, curPage);
+                // ...
+                markup += `<span class='pagination__dots'>...</span>`;
+                // last-3 -2 -1 last (for maxPagesTogether = 4)
+                for(let i = numOfPages - (maxPagesTogether - 1); i <= numOfPages; i++)markup += this._generateMarkupButton(i, curPage);
+            } else // render 1-last
+            for(let i = 1; i <= numOfPages; i++)markup += this._generateMarkupButton(i, curPage);
+        } else {
+            if (numOfPages > minPageLimit) {
+                // render 1
+                markup += this._generateMarkupButton(1, curPage);
+                // ...
+                markup += `<span class='pagination__dots'>...</span>`;
+                // -1 cur +1
+                for(let i = curPage - 1; i <= curPage + 1; i++)markup += this._generateMarkupButton(i, curPage);
+                // ...
+                markup += `<span class='pagination__dots'>...</span>`;
+                // last
+                markup += this._generateMarkupButton(numOfPages, curPage);
+            } else // render 1-last
+            for(let i = 1; i <= numOfPages; i++)markup += this._generateMarkupButton(i, curPage);
+        }
+        return markup;
+    }
+    _generatePrevNextButton(type, page, total) {
         return `
-        <button data-goto="${page}" class="btn--inline pagination__btn--${type}">
-            ${type === "next" ? `<span>Page ${page}</span>` : ""}
-            <svg class="search__icon">
-                <use href="${0, _iconsSvgDefault.default}#icon-arrow-${type === "prev" ? "left" : "right"}"></use>
-            </svg>
-            ${type === "prev" ? `<span>Page ${page}</span>` : ""}
+      <button data-goto="${page}" class="btn--inline pagination__btn--${type} ${type === "prev" && page <= 0 ? "hidden" : type === "next" && page > total ? "hidden" : ""}">
+        <svg class="search__icon">
+          <use href="${0, _iconsSvgDefault.default}#icon-arrow-${type === "prev" ? "left" : "right"}"></use>
+        </svg>
+      </button>
+    `;
+    }
+    _generateMarkupButton(page, cur) {
+        return `
+        <button data-goto="${page}" class="btn--inline ${cur === page ? "btn--active" : ""}">
+          ${page}
         </button>
     `;
     }
--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -9,40 +9,134 @@ class PaginationView extends View {
       this._data.results.length / this._data.resultsPerPage
     );
     const curPage = this._data.page;
+    let markup = '';
+    // Min number of pages after which (1 2 3 4 ... 7) pattern applies
+    const minPageLimit = 6;
+    // 1 2 3 4 ... 25 or 1 ... 22 23 24 25 (for maxPagesTogether = 4)
+    const maxPagesTogether = 4;
     // console.log(this._data);
-    // Page 1 and there are other pages
-    if (curPage === 1 && numOfPages > 1) {
-      //   return '1st page, others';
-      // show only next button
-      return this._generateMarkupButton('next', curPage + 1);
+
+    // If only one page no pagination
+    if (numOfPages === 1) {
+      return markup;
     }
-    // Last page
-    if (curPage === numOfPages && numOfPages > 1) {
-      // show only previous button
-      return this._generateMarkupButton('prev', curPage - 1);
-    }
-    // Other page
-    if (curPage < numOfPages && curPage > 1) {
-      //   return 'other page';
-      // show previous and next button
-      return (
-        this._generateMarkupButton('next', curPage + 1) +
-        this._generateMarkupButton('prev', curPage - 1)
-      );
-    }
-    // Page 1 and there are NO other pages
-    return '';
+
+    // Generate previous button
+    markup += this._generatePrevNextButton('prev', curPage - 1, numOfPages);
+
+    // Generate button for each page (numOfPages)
+    // for (let i = 1; i <= numOfPages; i++) {
+    //   markup += this._generateMarkupButton(i, curPage);
+    // }
+
+    // Generate Pagination Pattern (1... 5 6 7 ... 11)
+    markup += this._generatePaginationPattern(
+      curPage,
+      numOfPages,
+      minPageLimit,
+      maxPagesTogether
+    );
+
+    // Generate next button
+    markup += this._generatePrevNextButton('next', curPage + 1, numOfPages);
+    return markup;
   }
-  _generateMarkupButton(type, page) {
+  _generatePaginationPattern(
+    curPage,
+    numOfPages,
+    minPageLimit,
+    maxPagesTogether
+  ) {
+    let markup = '';
+    // 1 2 3 4 ... 25
+    if (curPage - maxPagesTogether < 1) {
+      if (numOfPages > minPageLimit) {
+        // render 1-4 (for maxPagesT = 4)
+        for (let i = 1; i <= maxPagesTogether; i++) {
+          markup += this._generateMarkupButton(i, curPage);
+        }
+        // ...
+        markup += `<span class='pagination__dots'>...</span>`;
+        // last
+        markup += this._generateMarkupButton(numOfPages, curPage);
+      } else {
+        // render 1-last
+        for (let i = 1; i <= numOfPages; i++) {
+          markup += this._generateMarkupButton(i, curPage);
+        }
+      }
+    }
+    // 1 ... 22 23 24 25
+    else if (curPage + maxPagesTogether > numOfPages) {
+      if (numOfPages > minPageLimit) {
+        // render 1
+        markup += this._generateMarkupButton(1, curPage);
+        // ...
+        markup += `<span class='pagination__dots'>...</span>`;
+        // last-3 -2 -1 last (for maxPagesTogether = 4)
+        for (
+          let i = numOfPages - (maxPagesTogether - 1);
+          i <= numOfPages;
+          i++
+        ) {
+          markup += this._generateMarkupButton(i, curPage);
+        }
+      } else {
+        // render 1-last
+        for (let i = 1; i <= numOfPages; i++) {
+          markup += this._generateMarkupButton(i, curPage);
+        }
+      }
+    }
+    // 1 ... 12 13 14 ... 25
+    // curPage = 13 (e.g)
+    else {
+      if (numOfPages > minPageLimit) {
+        // render 1
+        markup += this._generateMarkupButton(1, curPage);
+        // ...
+        markup += `<span class='pagination__dots'>...</span>`;
+        // -1 cur +1
+        for (let i = curPage - 1; i <= curPage + 1; i++) {
+          markup += this._generateMarkupButton(i, curPage);
+        }
+        // ...
+        markup += `<span class='pagination__dots'>...</span>`;
+        // last
+        markup += this._generateMarkupButton(numOfPages, curPage);
+      } else {
+        // render 1-last
+        for (let i = 1; i <= numOfPages; i++) {
+          markup += this._generateMarkupButton(i, curPage);
+        }
+      }
+    }
+
+    return markup;
+  }
+  _generatePrevNextButton(type, page, total) {
     return `
-        <button data-goto="${page}" class="btn--inline pagination__btn--${type}">
-            ${type === 'next' ? `<span>Page ${page}</span>` : ''}
-            <svg class="search__icon">
-                <use href="${icons}#icon-arrow-${
+      <button data-goto="${page}" class="btn--inline pagination__btn--${type} ${
+      type === 'prev' && page <= 0
+        ? 'hidden'
+        : type === 'next' && page > total
+        ? 'hidden'
+        : ''
+    }">
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-${
       type === 'prev' ? 'left' : 'right'
     }"></use>
-            </svg>
-            ${type === 'prev' ? `<span>Page ${page}</span>` : ''}
+        </svg>
+      </button>
+    `;
+  }
+  _generateMarkupButton(page, cur) {
+    return `
+        <button data-goto="${page}" class="btn--inline ${
+      cur === page ? 'btn--active' : ''
+    }">
+          ${page}
         </button>
     `;
   }
